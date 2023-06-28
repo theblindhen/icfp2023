ee917d95b48066a7a1902a59589221958a0e4b2f
Kasper Svendsen <kaspersv@gmail.com>
2023-06-28 18:35:16 +0200

    add ocaml-canvas example

---
+++ warmup/bin/canvas.ml
@@ -0,0 +1,71 @@
+open OcamlCanvas.V1
+
+let () =
+
+  Backend.init ();
+
+  let c = Canvas.createOnscreen ~title:"Hello world"
+            ~pos:(300, 200) ~size:(300, 200) () in
+
+  Canvas.setFillColor c Color.orange;
+  Canvas.fillRect c ~pos:(0.0, 0.0) ~size:(300.0, 200.0);
+
+  Canvas.setStrokeColor c Color.cyan;
+  Canvas.setLineWidth c 10.0;
+  Canvas.clearPath c;
+  Canvas.moveTo c (5.0, 5.0);
+  Canvas.lineTo c (295.0, 5.0);
+  Canvas.lineTo c (295.0, 195.0);
+  Canvas.lineTo c (5.0, 195.0);
+  Canvas.closePath c;
+  Canvas.stroke c;
+
+  Canvas.setFont c "Liberation Sans" ~size:36.0
+    ~slant:Font.Roman ~weight:Font.bold;
+
+  Canvas.setFillColor c (Color.of_rgb 0 64 255);
+  Canvas.setLineWidth c 1.0;
+  Canvas.save c;
+  Canvas.translate c (150.0, 100.0);
+  Canvas.rotate c (-. Const.pi_8);
+  Canvas.fillText c "Hello world !" (-130.0, 20.0);
+  Canvas.restore c;
+
+  Canvas.show c;
+
+  let e1 =
+    React.E.map (fun { Event.canvas = _; timestamp = _; data = () } ->
+        Backend.stop ()
+      ) Event.close
+  in
+
+  let e2 =
+    React.E.map (fun { Event.canvas = _; timestamp = _;
+                       data = { Event.key; char = _; flags = _ }; _ } ->
+        if key = KeyEscape then
+          Backend.stop ()
+      ) Event.key_down
+  in
+
+  let e3 =
+    React.E.map (fun { Event.canvas = _; timestamp = _;
+                       data = { Event.position = (x, y); button = _ } } ->
+        Canvas.setFillColor c Color.red;
+        Canvas.clearPath c;
+        Canvas.arc c ~center:(float_of_int x, float_of_int y)
+          ~radius:5.0 ~theta1:0.0 ~theta2:(2.0 *. Const.pi) ~ccw:false;
+        Canvas.fill c ~nonzero:false
+      ) Event.button_down
+  in
+
+  let frames = ref 0L in
+
+  let e4 =
+    React.E.map (fun { Event.canvas = _; timestamp = _; data = _ } ->
+        frames := Int64.add !frames Int64.one
+      ) Event.frame
+  in
+
+  Backend.run (fun () ->
+      ignore e1; ignore e2; ignore e3; ignore e4;
+      Printf.printf "Displayed %Ld frames. Goodbye !\n" !frames)
