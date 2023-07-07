e9f97759b6d86c34f6bb3d6083539a1933400ebb
Johan Rosenkilde <johanrosenkilde@github.com>
2023-07-07 14:53:06 +0200

    Formatting

--- contest/lib/types.ml
+++ contest/lib/types.ml
@@ -1,15 +1,11 @@
-type attendee = {
-  x: float;
-  y: float;
-  tastes: float list
-}
+type attendee = { x : float; y : float; tastes : float list }
 
 type problem = {
-  room_width: float;
-  room_height: float;
-  stage_width: float;
-  stage_height: float;
-  stage_bottom_left: float * float;
-  musicians: int list;
-  attendees: attendee list
+  room_width : float;
+  room_height : float;
+  stage_width : float;
+  stage_height : float;
+  stage_bottom_left : float * float;
+  musicians : int list;
+  attendees : attendee list;
 }
