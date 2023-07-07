a7e2725fff7970f2387bfa1a9d4d429800f0421d
Jonas Jensen <jbj@knef.dk>
2023-07-07 19:01:17 +0200

    Write solution to dir from main

--- contest/bin/main.ml
+++ contest/bin/main.ml
@@ -1,8 +1,7 @@
 open Core
 open Contest
-open Contest.Types
 
-let get_solution (p : problem) = Random_solver.random_placements p
+let get_solution (p : Types.problem) = Random_solver.random_placements p
 
 let () =
   let args = Sys.get_argv () in
@@ -12,10 +11,8 @@ let () =
   | Some problem ->
       print_endline (List.length problem.musicians |> string_of_int);
       let solution = get_solution problem in
-      let submission = Json_util.make_submission problem_id solution in
       let score = Score.score_solution problem solution in
       printf "Expected score: %f\n" score;
-      let out_file = "solution.json" in
+      Json_util.write_solution_if_positive problem_id problem solution;
       (* write solution_json to file *)
-      Out_channel.write_all out_file ~data:(Json_j.string_of_json_submission_post submission);
       print_endline "Done"
--- contest/lib/json_util.ml
+++ contest/lib/json_util.ml
@@ -11,3 +11,20 @@ let make_submission (problem_id : int) (solution : Types.solution) : Json_j.json
   in
   let submission : Json_j.json_submission_post = { problem_id; contents = solution_json } in
   submission
+
+(** Write the solution to the "../problems/solutions-%d.json" directory. For
+ * now, each file is named "positive.json" because we only write out the file if
+ * the score is positive. *)
+let write_solution_if_positive (problem_id : int) problem (solution : Types.solution) : unit =
+  let score : float = Score.score_solution problem solution in
+  if Float.is_positive score then (
+    let solution_json =
+      solution |> Types.json_solution_of_solution |> Json_j.string_of_json_solution
+    in
+    (* Create the directory "../problems/solutions-%d.json" if it doesn't exist. *)
+    let dir_name = sprintf "../problems/solutions-%d" problem_id in
+    (match Sys_unix.is_directory dir_name with
+    | `No -> Caml_unix.mkdir dir_name 0o777
+    | _ -> ());
+    let filename = sprintf "%s/positive.json" dir_name in
+    Out_channel.write_all filename ~data:solution_json)
