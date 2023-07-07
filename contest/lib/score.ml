1d7216a0a236684f7dd2c2706ed14540db3652e8
Johan Rosenkilde <johanrosenkilde@github.com>
2023-07-07 20:29:44 +0200

    Scoring with block integrated

--- contest/lib/score.ml
+++ contest/lib/score.ml
@@ -1,12 +1,19 @@
+open Core
 open Types
 open Misc
 
-let score_I (a : attendee) (m : musician) : float =
-  let d = distance a.pos m.pos in
-  ceil (1_000_000.0 *. a.tastes.(m.instrument) /. (d *. d))
+let is_blocked (s : solution) (a : attendee) (m : musician) : bool =
+  let within_distance = Geometry.within_distance 5.0 (a.pos, m.pos) in
+  Array.exists ~f:(fun candidate -> m.id <> candidate.id && within_distance candidate.pos) s
+
+let score_I (s : solution) (a : attendee) (m : musician) : float =
+  if is_blocked s a m then 0.0
+  else
+    let d = distance a.pos m.pos in
+    Float.round_up (1_000_000.0 *. a.tastes.(m.instrument) /. (d *. d))
 
 let score_attendee (s : solution) (a : attendee) : float =
-  Array.fold_left (fun sum m -> sum +. score_I a m) 0.0 s
+  Array.sum (module Float) ~f:(score_I s a) s
 
 let score_solution (p : problem) (s : solution) : float =
-  List.fold_left (fun sum attendee -> sum +. score_attendee s attendee) 0.0 p.attendees
+  List.sum (module Float) ~f:(score_attendee s) p.attendees
