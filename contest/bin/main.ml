8da423bd0b0d75707524cdab964e23dfcad923f8
Kasper Svendsen <kaspersv@gmail.com>
2023-07-07 16:44:36 +0200

    Run random solver

--- contest/bin/main.ml
+++ contest/bin/main.ml
@@ -6,9 +6,7 @@ let get_problem problem_id =
   let json = In_channel.read_all ("../problems/problem-" ^ string_of_int problem_id ^ ".json") in
   json |> Json_j.json_problem_of_string |> problem_of_json_problem
 
-let get_solution () =
-  let solution : solution = [ { x = 1.; y = 2.5 } ] in
-  solution
+let get_solution (p : problem) = Random_solver.random_placements p
 
 let make_submission (problem_id : int) (solution : solution) : Json_j.json_submission_post =
   let solution_json = solution |> json_solution_of_solution |> Json_j.string_of_json_solution in
@@ -19,8 +17,10 @@ let () =
   let args = Sys.get_argv () in
   let problem = get_problem 1 in
   print_endline (List.length problem.musicians |> string_of_int);
-  let solution = get_solution () in
+  let solution = get_solution problem in
   let submission = make_submission 1 solution in
+  let score = Score.score_solution problem solution in
+  printf "Expected score: %f\n" score;
   let out_file = args.(1) in
   (* write solution_json to file *)
   Out_channel.write_all out_file ~data:(Json_j.string_of_json_submission_post submission);
