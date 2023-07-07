5a58c4d6e669ce2da8e9d2a20d1815acd23e15c6
Jonas Jensen <jbj@knef.dk>
2023-07-07 17:41:14 +0200

    Take problem id from command line

--- contest/bin/main.ml
+++ contest/bin/main.ml
@@ -6,15 +6,16 @@ let get_solution (p : problem) = Random_solver.random_placements p
 
 let () =
   let args = Sys.get_argv () in
-  match Json_util.get_problem 1 with
+  let problem_id = args.(1) |> Int.of_string in
+  match Json_util.get_problem problem_id with
   | None -> failwith "Failed to parse problem"
   | Some problem ->
       print_endline (List.length problem.musicians |> string_of_int);
       let solution = get_solution problem in
-      let submission = Json_util.make_submission 1 solution in
+      let submission = Json_util.make_submission problem_id solution in
       let score = Score.score_solution problem solution in
       printf "Expected score: %f\n" score;
-      let out_file = args.(1) in
+      let out_file = "solution.json" in
       (* write solution_json to file *)
       Out_channel.write_all out_file ~data:(Json_j.string_of_json_submission_post submission);
       print_endline "Done"
