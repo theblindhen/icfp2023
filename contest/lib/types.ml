0602237d0a9d077625b050b1e885357e0713728f
Johan Rosenkilde <johanrosenkilde@github.com>
2023-07-07 14:18:09 +0200

    Problem input data structure

--- contest/lib/types.ml
+++ contest/lib/types.ml
@@ -1,1 +1,15 @@
-type monkey = Ape of string | Human of string
+type attendee = {
+  x: float;
+  y: float;
+  tastes: float list
+}
+
+type problem = {
+  room_width: float;
+  room_height: float;
+  stage_width: float;
+  stage_height: float;
+  stage_bottom_left: float * float;
+  musicians: int list;
+  attendees: attendee list
+}
