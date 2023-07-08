d0df84e77e6f0c5d9d9762344db9bc07c0ea6df1
Kasper Svendsen <kaspersv@gmail.com>
2023-07-08 11:39:04 +0200

    Add edge placer

--- contest/bin/main.ml
+++ contest/bin/main.ml
@@ -1,17 +1,21 @@
 open Core
 open Contest
 
-let random_solution (p : Types.problem) = Random_solver.random_placement_solution p
+let random_solution (p : Types.problem) (already_placed : Types.position list) =
+  Random_solver.random_placement_solution p already_placed
 
 type assignment = LP | Swap | Random [@@deriving sexp]
-type invocation = { problem_id : int; assignment : assignment } [@@deriving sexp]
+
+type invocation = { problem_id : int; assignment : assignment; edges : Edge_placer.edges }
+[@@deriving sexp]
 
 let run_invocation inv =
   match Json_util.get_problem inv.problem_id with
   | None -> failwith "Failed to parse problem"
   | Some problem ->
       print_endline (List.length problem.musicians |> string_of_int);
-      let solution = random_solution problem in
+      let edge = Edge_placer.place_edges problem inv.edges in
+      let solution = random_solution problem edge in
       let score = Score.score_solution problem solution in
       printf "Random solution score: %f\n" score;
       let optimized_solution =
@@ -21,6 +25,7 @@ let run_invocation inv =
         | LP -> Lp_solver.lp_optimize_solution problem solution
       in
       let optimised_score = Score.score_solution problem optimized_solution in
+      Types.validate_solution problem optimized_solution;
       printf "Improved score: %f\n" optimised_score;
       Json_util.write_solution_if_best inv.problem_id problem optimized_solution;
       (* write solution_json to file *)
@@ -30,9 +35,16 @@ let command =
   Command.basic ~summary:"Run our solver on a problem"
     (let%map_open.Command lp = flag "--lp" no_arg ~doc:"Use the LP solver after placement"
      and swapper = flag "--swap" no_arg ~doc:"Use swap optimization after placement"
+     and edge = flag "--edge" (optional string) ~doc:"Edge placement"
      and problem_id = anon ("problem_id" %: string) in
      fun () ->
        let problem_id = Int.of_string problem_id in
+       let edges =
+         match edge with
+         | Some "north" -> [ Edge_placer.North ]
+         | Some "south" -> [ Edge_placer.South ]
+         | _ -> []
+       in
        let assignment =
          if lp && swapper then failwith "Can't use both LP and swap"
          else if lp then (
@@ -43,6 +55,6 @@ let command =
            Swap)
          else Random
        in
-       run_invocation { problem_id; assignment })
+       run_invocation { problem_id; assignment; edges })
 
 let () = Command_unix.run command
--- contest/lib/random_solver.ml
+++ contest/lib/random_solver.ml
@@ -1,5 +1,6 @@
 open Types
 open Misc
+open Core
 
 let random_placement (p : problem) : position =
   let x_offset = Random.float (p.stage_width -. 20.0) in
@@ -7,9 +8,9 @@ let random_placement (p : problem) : position =
   { x = x_offset +. 10.0 +. p.stage_bottom_left.x; y = y_offset +. 10.0 +. p.stage_bottom_left.y }
 
 let is_valid_placement (_ : problem) (placed : position list) (potential : position) : bool =
-  placed |> List.for_all (fun x -> distance x potential >= 10.0)
+  placed |> List.for_all ~f:(fun x -> Float.(distance x potential >= 10.0))
 
-let random_placements (p : problem) : position list =
+let random_placements (p : problem) (already_placed : position list) : position list =
   Random.self_init ();
   let rec random (acc : position list) (toPlace : int list) (fuel : int) =
     match toPlace with
@@ -17,10 +18,10 @@ let random_placements (p : problem) : position list =
     | _ :: t ->
         let potential = random_placement p in
         if is_valid_placement p acc potential then random (potential :: acc) t fuel
-        else if fuel == 0 then failwith "ran out of fuel"
+        else if fuel = 0 then failwith "ran out of fuel"
         else random acc toPlace (fuel - 1)
   in
-  random [] p.musicians 10_000
+  random already_placed (List.drop p.musicians (List.length already_placed)) 10_000
 
-let random_placement_solution (p : problem) : solution =
-  random_placements p |> solution_of_positions p
+let random_placement_solution (p : problem) (already_placed : position list) : solution =
+  random_placements p already_placed |> solution_of_positions p
