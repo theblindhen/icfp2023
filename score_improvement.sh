e3333a04b55b3bd0c9fad08fbf3c05a66e1f11c7
Johan Rosenkilde <johanrosenkilde@github.com>
2023-07-08 10:53:25 +0200

    improvements to script

--- score_improvement.sh
+++ score_improvement.sh
@@ -7,11 +7,21 @@ for p in {1..55}; do
     # Submitted solutions are <score>.json.submitted
     # Print the best score and previous best score
     BEST=$(ls | grep '^[0-9]*[.]json$' | sed 's/[.]json$//' | sort -n | tail -n 1);
-    BEST_SUBMITTED=$(ls | grep '^[0-9]*[.]json[.]submitted$' | sed 's/[.]json[.]submitted$//' | sort -n | tail -n 1);
+    # If there are any submitted
+    if [ -n "$(ls | grep '^[0-9]*[.]json[.]submitted$')" ]; then
+      # Print the best submitted score
+      BEST_SUBMITTED=$(ls | grep '^[0-9]*[.]json[.]submitted$' | sed 's/[.]json[.]submitted$//' | sort -n | tail -n 1);
+      NEW_SOLUTION="";
+    else
+      # Otherwise, print 0
+      BEST_SUBMITTED=0;
+      NEW_SOLUTION="  new solution!";
+    fi;
     # If BEST is better than BEST_SUBMITTED then print
     if [ "$BEST" -gt "$BEST_SUBMITTED" ]; then
       IMPROVEMENT=$(($BEST - $BEST_SUBMITTED));
-      echo "$p  :  $IMPROVEMENT"
+      if [ 
+      echo "$p: improved by $IMPROVEMENT $NEW_SOLUTION"
     fi;
     popd
   else
