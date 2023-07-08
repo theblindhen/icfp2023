ed7d52dbc20ce6680f92b77a3e0d8051fb996254
Johan Rosenkilde <johanrosenkilde@github.com>
2023-07-08 20:22:58 +0200

    Rm unbuilding code

--- contest/lib/physics.ml
+++ contest/lib/physics.ml
@@ -4,6 +4,6 @@ open Geometry
 
 type force = { x : float; y : float }
 
-let force_I (p : problem) (a : attendee) (i : instrument) : float =
-  let d_sq = p a in
-  Float.round_up (1_000_000.0 *. a.tastes.(i) /. d_sq)
+(* let force_I (p : problem) (a : attendee) (i : instrument) : float =
+   let d_sq = p a in
+   Float.round_up (1_000_000.0 *. a.tastes.(i) /. d_sq) *)
