bdff8227a7b49696eff2156e244e4197f118a5a1
MrBlindChicken <112624925+MrBlindChicken@users.noreply.github.com>
2023-07-08 18:16:12 +0000

    began physics

---
+++ contest/lib/physics.ml
@@ -0,0 +1,9 @@
+open Core
+open Types
+open Geometry
+
+type force = { x : float; y : float }
+
+let force_I (p : problem) (a : attendee) (i : instrument) : float =
+  let d_sq = p a in
+  Float.round_up (1_000_000.0 *. a.tastes.(i) /. d_sq)
