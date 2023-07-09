a5e3942a84faea6ec42714c6b1ca786c939b5abe
Jonas Jensen <jbj@knef.dk>
2023-07-09 21:28:44 +0200

    New swaptimization that accounts for q

--- contest/lib/improver.ml
+++ contest/lib/improver.ml
@@ -2,6 +2,7 @@ open Core
 open Types
 
 type musician_score = { mutable score : float; mutable musician : musician }
+type musician_score_split = { mutable position_idx : int; instrument : int }
 
 let scores_of_musicians (env : Score.scoring_env) : musician_score array =
   Array.map env.solution ~f:(fun m -> { score = Score.score_musician env m; musician = m })
@@ -63,3 +64,76 @@ let swapper_without_q (p : problem) (s : solution) ~(round : int) : solution =
   done;
   printf "Score should be: %f\n%!" (Array.sum (module Float) scores ~f:(fun s -> s.score));
   Array.map scores ~f:(fun s -> s.musician)
+
+let swapper_with_q (p : problem) (s : solution) ~(round : int) : solution =
+  ignore round;
+  (* Sort s by id so there's no confusion. *)
+  let s = Array.sorted_copy s ~compare:(fun a b -> Int.compare a.id b.id) in
+  (* We introduce the concept of a _musician id_, which refers to the index into
+   * `s` (aka the `id` field of items in `s`). *)
+  (* A _position index_ is also an index into `s`, denoting the `pos` found
+   * there. *)
+  let env = Score.get_scoring_env p s in
+  let cache = score_cache env in
+  (* Indexed by _musician_ id *)
+  let scores = Array.mapi s ~f:(fun i m -> { position_idx = i; instrument = m.instrument }) in
+  (* Lookup table of _position_ index pairs to q fractions (if these positions
+   * should have the same musician on them). *)
+  let q_term_map =
+    Array.mapi s ~f:(fun i m ->
+        Array.mapi s ~f:(fun i' m' -> if i = i' then 0.0 else 1. /. Geometry.distance m.pos m'.pos))
+  in
+  (* For each _musician_ index, list the _musician_ indexes of other musicians
+   * with the same instrument. *)
+  let other_musicians = Array.create ~len:(Array.length s) [] in
+  Array.to_list s
+  |> List.sort_and_group ~compare:(fun a b -> Int.compare a.instrument b.instrument)
+  |> List.iter ~f:(fun l ->
+         List.iter l ~f:(fun m ->
+             other_musicians.(m.id) <-
+               List.filter_map l ~f:(fun m' -> if m'.id <> m.id then Some m'.id else None)));
+  (* Gets the hypothetical contribution of all musicials with the same
+   * instrument as musician i if i were located at the current position of musician
+   * j. *)
+  let get_score i j =
+    let pos_idx = scores.(j).position_idx in
+    let instrument = scores.(i).instrument in
+    let get_fake_pos_idx i' = if i' = i then pos_idx else scores.(i').position_idx in
+    let musicians_with_instrument = i :: other_musicians.(i) in
+    List.sum
+      (module Float)
+      musicians_with_instrument
+      ~f:(fun i ->
+        (* shadowing i *)
+        let audience_contribution =
+          Hashtbl.Poly.find_exn cache (s.(get_fake_pos_idx i).pos, instrument)
+        in
+        let q_factor =
+          1.
+          +. List.sum
+               (module Float)
+               other_musicians.(i)
+               ~f:(fun i' -> q_term_map.(get_fake_pos_idx i).(get_fake_pos_idx i'))
+        in
+        q_factor *. audience_contribution)
+  in
+  let iter = ref true in
+  while !iter do
+    iter := false;
+    for i = 0 to Array.length scores - 1 do
+      for j = i + 1 to Array.length scores - 1 do
+        if scores.(i).instrument <> scores.(j).instrument then
+          let score_i = get_score i i in
+          let score_j = get_score j j in
+          let score_i' = get_score i j in
+          let score_j' = get_score j i in
+          if Float.(score_i' + score_j' > score_i + score_j) then (
+            printf "swapping %d and %d\n%!" i j;
+            iter := true;
+            let tmp = scores.(i).position_idx in
+            scores.(i).position_idx <- scores.(j).position_idx;
+            scores.(j).position_idx <- tmp)
+      done
+    done
+  done;
+  Array.map s ~f:(fun m -> { m with pos = s.(scores.(m.id).position_idx).pos })
